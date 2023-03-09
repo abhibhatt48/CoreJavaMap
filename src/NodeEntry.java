244ef6f36940ca9bf69ce4639e71dd3bfb436fef
Abhishek Bhatt <47849440+abhibhatt48@users.noreply.github.com>
2023-03-09 13:10:45 -0400

    This class is used to set the priority queue for the route.

---
+++ src/NodeEntry.java
@@ -0,0 +1,11 @@
+
+// node entry class for determine the priority in priority queue
+public class NodeEntry {
+        Node node;
+        int priority;
+
+        public NodeEntry(Node node, int priority) {
+            this.node = node;
+            this.priority = priority;
+        }
+    }
