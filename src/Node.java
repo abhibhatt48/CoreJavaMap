85625fd50314ebcd5186d74173ba5e7b0327addb
Abhishek Bhatt <47849440+abhibhatt48@users.noreply.github.com>
2023-03-09 12:59:47 -0400

    This class is used to get and set Street and use to set x and y position for Point class.

---
+++ src/Node.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+        Point position;
+        String label;
+        private List<StreetEntry> connectedStreets = new ArrayList<>();
+
+        public List<StreetEntry> getStreets() {
+            return connectedStreets;
+        }
+
+        Node(int x, int y) {
+            this.position = new Point(0, 0);
+            this.label = x + "_" + y;
+        }
+
+        public boolean addStreet(String streetId, Point from, Point to) {
+            try {
+                connectedStreets.add(new StreetEntry(streetId, this.position, to));
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }
+
+    }
