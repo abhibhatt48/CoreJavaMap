62d9db11d13116ba49134dd72f0bd043a15a20de
Abhishek Bhatt <47849440+abhibhatt48@users.noreply.github.com>
2023-03-10 02:01:58 -0400

    Added extra enums.

--- src/TurnDirection.java
+++ src/TurnDirection.java
@@ -2,5 +2,5 @@
  * Set of possible turn types from one street to another.
  */
 public enum TurnDirection {
-    Left, Right, Straight, UTurn
+    Left, Right, Straight, UTurn, Slight_Left, Slight_Right, Sharp_Left, Sharp_Right
 }
